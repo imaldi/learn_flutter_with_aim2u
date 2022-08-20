cad22475b17ab287ad22e1ea7f6ce283ea9ecc4c
imaldi <aldiirsanmajid@gmail.com>
2022-08-21 02:54:47 +0700

    Pengenalan Struktur Basic Aplikasi Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 
+/// 1. main() function ini adalah entry point aplikasi flutter,
+/// kadang disini ada kode inisiasi lainnya selain dari runApp()
 void main() {
+  /// di runApp ini Widget paling root dijalankan, disini namanya masih MyApp, bisa diubah kalau perlu
   runApp(const MyApp());
 }
 
+/// Root Widget mereturn MaterialApp
+/// MaterialApp adalah yang memberikan App kita design2 basic (global) dari sebuah aplikasi flutter
+/// disini tempat utama kita nentukan style UI app kita dan juga termasuk route nya
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -11,88 +17,80 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      /// 1. title => Nama App yang tampil di Icon Launcher
+      title: 'Flutter with AIM2U',
+      /// 2. debugShowCheckedModeBanner => banner penanda aplikasi dalam development, set false jika untuk prod
+      debugShowCheckedModeBanner: false,
+      /// 3. theme => disini kita bisa mengatur theme
+      /// (primary color, secondary color, accent color, font size dll) global untuk lightmode kita
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        /// maksud dari primary swatch ini warnanya ada 10 shade yg bisa dipilih, defaultnya 500,
+        /// bisa kita pilih dari sekian banyak swatch yg disediakan flutter atau buat sendiri
+        /// dengan tipe data MaterialColor dan simpan jadi konstanta di suatu tempat
         primarySwatch: Colors.blue,
       ),
+      /// 4. darktheme => disini kita bisa mengatur theme (primary color, secondary color, accent color, font size)untuk Dark Mode aplikasi kita
+      /// kalau tidak diset, maka yang tampil theme normal (Light Mode)
+      // darkTheme: ThemeData(
+      //   primarySwatch: Colors.green,
+      // ),
+      /// Route
+      /// 5. home => ini adalah home Route aplikasi kita
+      /// Disini kita bisa menentukan route kita dengan beberapa cara,
+      /// untuk saat ini kita pakai cara default saja dulu
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
+/// 6. Stateful dan stateless
+/// Kalau kita mengibaratkan aplikasi flutter dengan bahasa yang simple sebagai sebuah papan iklan,
+/// stateless widget adalah banner iklan yang tidak bisa diubah kontennya setelah diprint
+/// sedangkan,
+/// stateful widget adalah banner iklan yang ditempatkan di layar LCD yang iklannya lebih dinamis dan bisa berubah,
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  /// Widget ini adalah homepage aplikasi kita. Widgetnya stateful yang artinya bisa berubah seperti yang sudah dijelaskan tadi
+  /// Nilai2 awal / konfigurasi dari constructor dalam widget stateful selalu disimpan sebagai variabel final
   final String title;
 
+  /// Kalau widget stateful, dia override method createState() yang mereturn class yang Implement Class State
+  /// nah di class State tersebutlah nilai2 yang berubah dari suatu widget di manipulasi
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  /// nah ini contoh variable state nya
   int _counter = 0;
 
+  /// ini function yang merubah state dan di panggil di salah satu method lifecycle dari State ex: initState, build, dispose, etc
   void _incrementCounter() {
+    /// setState ini ibarat tombol refresh untuk StatefulWidget, setiap dipanggil akan merefresh widget
+    /// kalau kita cuma ubah value tanpa panggil setState, tampilan di layar ga akan berubah karena method build ga terpanggil lagi
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
+  /// 7. build method
+  /// ini lifecycle utama dari aplikasi Flutter, tempat widget benar2 di build untuk tampil di layar,
+  /// method ini terpanggil setiap kali setState dipanggil, contohnya seperti di method _incrementCounter di atas
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    /// 8. Scaffold ini biasanya dipakai untuk membuat satu page, karena didalamnya disediakan hal2 dasar dari suatu page,
+    /// seperti appbar, body, floating action button, bottom navigation bar, dll
     return Scaffold(
+      /// 9. Appbar ya taulah ya
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        /// disini kita ambil value dari MyHomePage widget, mengaksesnya dengan getter widget.title
         title: Text(widget.title),
       ),
+      /// 10. body ini inti konten halaman ini
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -105,6 +103,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+      /// 11. floating action button ini optional, kalau ga mau bisa dihilangin aja
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
